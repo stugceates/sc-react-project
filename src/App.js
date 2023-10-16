2387ce6a389d5868509195276630f6699824f763
stugceates <stugceates@gmail.com>
2023-10-16 14:30:11 +0100

    add github repo link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Selin Ates and is open sourced on{" "}
+        <a
+          href="https://github.com/stugceates/sc-react-project"
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
