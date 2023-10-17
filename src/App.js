423929a0c2dddb3940a7159aae7eb118aa63bfd0
stugceates <stugceates@gmail.com>
2023-10-17 13:05:19 +0100

    add bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Selin Ates and is open sourced on{" "}
-        <a
-          href="https://github.com/stugceates/sc-react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Selin Ates and is open sourced on{" "}
+          <a
+            href="https://github.com/stugceates/sc-react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
